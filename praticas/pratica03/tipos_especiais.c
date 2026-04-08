6265eb3f2788b3d7258cf7c902ec46e0806d0f28
alexandre izidro <alexandre.p.izidro@gmail.com>
2026-04-08 19:27:19 -0300

    feat: conclui pratica03. Fecha #5

---
+++ praticas/pratica03/tipos_especiais.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+
+    int numero_paises = 195;
+    int numero_idiomas = 7100;
+    long long int populacao_mundial = 8274065924LL; 
+    long double proporcao_aurea = 1.61803398874989484820;
+
+    printf("nuemro de paises: %i\n", numero_paises);
+    printf("numero de idiomas: %i\n", numero_idiomas);
+    printf("populacao mundial: %lld\n", populacao_mundial);
+    printf("proporcao aurea %.20Lf\n", proporcao_aurea);
+
+   
+    return 0;
+}
